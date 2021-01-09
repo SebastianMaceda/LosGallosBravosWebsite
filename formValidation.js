fe2098b9544555c265cec9dcee69052af582c3ff
SebastianMaceda <macedastm@gmail.com>
2021-01-08 19:46:29 -0600

    adding 'required' attribute to html

--- formValidation.js
+++ formValidation.js
@@ -6,20 +6,27 @@ const formMessageBox = document.querySelector('#messageBox');
 const submitBtn = document.querySelector('.submitBtn');
 
 
-submitBtn.addEventListener('click', formSubmit);
+submitBtn.addEventListener('submit', formSubmit);
 
 
 
 function formSubmit(e){
     e.preventDefault();
     if (formFname.value == "" || formFname.value == NaN){
+        // alert("Please Enter Your Full Name");
         console.log('yeep');
     }if (formEmailBox.value == "" || formEmailBox.value == NaN){
         console.log('yeep 1 ');
+        // alert("Please Enter Your Email");
+
     }if (formSubjectBox.value == "" || formSubjectBox.value == NaN){
         console.log('yeep 2');
+        // alert("Please Enter A Subject");
+
     }if (formMessageBox.value == "" || formMessageBox.value == NaN){
         console.log('yeep 3');
+        // alert("Please Enter Your Message");
+
     }
 
     
