943efb08c69fcd2c4a4bd4b3504e19a4abcc96f4
SebastianMaceda <51980363+SebastianMaceda@users.noreply.github.com>
2021-12-08 21:10:24 -0600

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,5 @@ const gymCopyright = document.querySelector('.losGallosBravosCopyright');
 
 
 let currentYear = new Date().getFullYear();
-let creator = 'https://www.linkedin.com/in/sebastian-maceda-25977a180/'
 
 gymCopyright.textContent = `© ${currentYear} Los Gallos Bravos Boxing. All Rights Reserved`
